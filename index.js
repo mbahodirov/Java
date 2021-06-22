179d43c4d50d347deb08074797ee4e2e95aaf6f4
mbahodirov <mbohodirov7@gmail.com>
2021-06-22 16:09:02 +0500

    js tayyor

---
+++ index.js
@@ -0,0 +1,44 @@
+
+// Numbers
+// String " ", " " , ""
+// Boolean true false
+// undefined
+// null
+// Array []
+// Object {}
+// Function
+// Symbols
+// var, let, const
+
+
+//P=4*a;
+var a = 15;
+var P = 4*a;
+console.log(P)
+console.log("=========")
+
+//S=a2
+var b = 5;
+// var s = b**2;
+var s = Math.pow(b,2);
+console.log(s);
+console.log("=========")
+
+//S=a*b; va P=2* (a+b)
+var c = 2;
+var d = 4;
+var S3 = c*d;
+console.log(S3);
+var P3 = 2*(c+d);
+console.log(P3)
+console.log("=========")
+
+//L-pi*d  pi=3.14
+var pi=3.14;
+var d=6;
+var p4 = pi*d;
+console.log(p4)
+console.log("=========")
+
+//V=a3   S=6*a2
+var 
