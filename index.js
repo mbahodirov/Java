fc34f657c72949948cb114b071da56e6d5b1d57e
mbahodirov <mbohodirov7@gmail.com>
2021-06-26 15:04:30 +0500

    homework3

--- index.js
+++ index.js
@@ -12,33 +12,65 @@
 
 
 //P=4*a;
-var a = 15;
-var P = 4*a;
-console.log(P)
-console.log("=========")
+// var a = 15;
+// var P = 4*a;
+// console.log(P)
+// console.log("=========")
 
-//S=a2
-var b = 5;
-// var s = b**2;
-var s = Math.pow(b,2);
-console.log(s);
-console.log("=========")
+// //S=a2
+// var b = 5;
+// // var s = b**2;
+// var s = Math.pow(b,2);
+// console.log(s);
+// console.log("=========")
 
-//S=a*b; va P=2* (a+b)
-var c = 2;
-var d = 4;
-var S3 = c*d;
-console.log(S3);
-var P3 = 2*(c+d);
-console.log(P3)
-console.log("=========")
+// //S=a*b; va P=2* (a+b)
+// var c = 2;
+// var d = 4;
+// var S3 = c*d;
+// console.log(S3);
+// var P3 = 2*(c+d);
+// console.log(P3)
+// console.log("=========")
 
-//L-pi*d  pi=3.14
-var pi=3.14;
-var d=6;
-var p4 = pi*d;
-console.log(p4)
-console.log("=========")
+// //L-pi*d  pi=3.14
+// var pi=3.14;
+// var d=6;
+// var p4 = pi*d;
+// console.log(p4)
+// console.log("=========")
 
-//V=a3   S=6*a2
-var
+// //V=a3   S=6*a2;
+// var i=4;
+// var V=i**3
+// console.log(V)
+// var S5 = 6*Math.pow(i,2);
+// console.log(S5)
+// console.log("=========")
+
+//a,b,c  V=a*b*c va
+
+let  grade=-5
+if(grade<100 && grade>89){
+    console.log('A+')
+}else if(grade<=89 && grade>=85){
+    console.log('A')
+}else if (grade<85 && grade>80){
+    console.log('A-');
+}else if (grade<80 && grade>=75){
+    console.log('B+');
+}else if (grade<74 && grade>=70){
+    console.log('B');
+}else if (grade<69 && grade>=65){
+    console.log('C+');
+}else if (grade<64 && grade>=60){
+    console.log('C');
+}else if (grade<59 && grade>=55){
+    console.log('D+');
+}else if (grade<54 && grade>=50){
+    console.log('D');
+}else if (grade<49 && grade>=40){
+    console.log('E');
+}else if (grade<39  && grade>=0){
+    console.log('F');
+}else {console.log('none');}
